eb4d40839da8e4a5746c3031c7d96d85b1249db7
Deepak Thakur <dthakur886210@gmail.com>
2022-09-25 23:31:53 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutCompanyComponent } from './about-company/about-company.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { NopageComponent } from './nopage/nopage.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'about',
+  component: AboutComponent,
+  children:[
+    {path : 'company', component: AboutCompanyComponent},
+    {path : 'me', component: AboutMeComponent}
+  ]
+  },
+  {path : 'user/:id',
+  component: UserComponent
+  },
+  {path : '',
+  component: HomeComponent
+  },
+  {path : '**',
+  component: NopageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
