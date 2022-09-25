06cdf2e9b54315b43839923b4347391f54261e1b
Deepak Thakur <dthakur886210@gmail.com>
2022-09-26 01:10:22 +0530

    shopping list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipiesListComponent } from './recipies/recipies-list/recipies-list.component';
+import { RecipiesDetailsComponent } from './recipies/recipies-details/recipies-details.component';
+import { RecipiesItemComponent } from './recipies/recipies-list/recipies-item/recipies-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipiesComponent,
+    RecipiesListComponent,
+    RecipiesDetailsComponent,
+    RecipiesItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
