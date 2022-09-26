93582571df66faacc0d922a8f6ce1798a14c161d
Deepak Thakur <dthakur886210@gmail.com>
2022-09-26 12:28:28 +0530

    OnInit, OnDestroy, Onchanges,DoCheck,AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked

--- src/app/cild/cild.component.ts
+++ src/app/cild/cild.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, OnChanges, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, OnChanges, Input, ContentChild } from '@angular/core';
 
 @Component({
   selector: 'app-cild',
@@ -9,6 +9,7 @@ export class CildComponent implements OnInit, OnDestroy, OnChanges {
   counter :number = 0;
   interval : any;
    @Input() channelName = "";
+   @ContentChild('projectedContent') projectedContent : any;
   constructor() { 
     console.log("child constructor is called");
     
@@ -16,6 +17,8 @@ export class CildComponent implements OnInit, OnDestroy, OnChanges {
 
   ngOnInit(): void {
     console.log("child oninit is called");
+    console.log("oninit -" + this.projectedContent);
+    
   //  this.interval =  setInterval(()=>{
   //     this.counter = this.counter +1;
   //     console.log(this.counter);
@@ -26,19 +29,46 @@ export class CildComponent implements OnInit, OnDestroy, OnChanges {
     //Add 'implements OnDestroy' to the class.
     clearInterval(this.interval);
     console.log("Child ondestroy is called ");
+    console.log("ondestroy -" + this.projectedContent);
     
   }
   ngOnChanges(): void {
     //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
     //Add '${implements OnChanges}' to the class.
     console.log("child onchanges is called");
+    console.log("onchanges -" + this.projectedContent);
     
   }
   ngDoCheck(): void {
     //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
     //Add 'implements DoCheck' to the class.
     console.log("Child docheck is called");
+    console.log("docheck -" + this.projectedContent);
     
   }
-
+  ngAfterContentInit(): void {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    //Add 'implements AfterContentInit' to the class.
+    console.log("in after content init");
+    console.log("ngAftercontentinit -" + this.projectedContent);
+    
+  }
+  ngAfterContentChecked(): void {
+    //Called after every check of the component's or directive's content.
+    //Add 'implements AfterContentChecked' to the class.
+    console.log("in aftercontentchecked");
+    
+  }
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    console.log("in afterviewInit");
+    
+  }
+ngAfterViewChecked(): void {
+  //Called after every check of the component's view. Applies to components only.
+  //Add 'implements AfterViewChecked' to the class.
+  console.log("in afterViewChecked");
+  
+}
 }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -3,33 +3,31 @@ import { Component, Input, OnChanges, OnInit } from '@angular/core';
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.css']
+  styleUrls: ['./parent.component.css'],
 })
 export class ParentComponent implements OnInit, OnChanges {
- isChild = true;
- channelName = '';
+  isChild = true;
+  channelName = '';
 
-  constructor() { console.log("parent constructor is called ");
-   }
+  constructor() {
+    console.log('parent constructor is called ');
+  }
 
   ngOnInit(): void {
-    console.log("parent oninit is called");
-    
+    console.log('parent oninit is called');
   }
   ngOnChanges(): void {
     //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
     //Add '${implements OnChanges}' to the class.
-    console.log("Parent onchanges is called");
-    
+    console.log('Parent onchanges is called');
   }
-  toggleChild(){
+  toggleChild() {
     this.isChild = !this.isChild;
   }
   ngDoCheck(): void {
     //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
     //Add 'implements DoCheck' to the class.
-    console.log("Parent docheck is called");
-    
+    console.log('Parent docheck is called');
   }
-
+  // thats why we should avoid the ngonchange and docheck lifecycle in one component, because they sometimes create the infinite loop
 }
