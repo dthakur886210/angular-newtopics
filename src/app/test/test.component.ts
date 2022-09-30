371457cbd2d4cd3c21d17f72cff74effa30dd100
Deepak Thakur <dthakur886210@gmail.com>
2022-10-01 01:29:35 +0530

    updated

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { TitleStrategy } from '@angular/router';
 
 @Component({
   selector: 'app-test',
